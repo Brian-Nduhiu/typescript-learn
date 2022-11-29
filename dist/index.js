050602b281a782949308c94af913f1606a1ab618
Brian Nduhiu <briannduhiu@gmail.com>
2022-11-29 10:19:39 +0300

    github copilot addition

--- dist/index.js
+++ dist/index.js
@@ -1,7 +1,9 @@
 "use strict";
-let age = 30;
-if (age < 50) {
-    age += 10;
+let age = 30_000_000;
+function nums(values) {
+    for (let i = 0; i < values.length; i++) {
+        console.log(values[i].toString());
+    }
 }
-console.log(age);
+nums([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,11 @@
 
-let age : number = 30
+let age= 30_000_000;
 
-if (age < 50) {
-    age += 10
+function nums(values:number[]) {
+    //loop through the array and convert each value to a string and log it
+    for (let i = 0; i < values.length; i++) {     
+        console.log(values[i].toString());
+    }
 }
 
-console.log(age);
+nums([1,2,3,4,5,6,7,8,9,10]);
