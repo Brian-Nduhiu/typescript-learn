33cf063804b68828fcef216ac24e9195f4d65715
Brian Nduhiu <briannduhiu@gmail.com>
2022-11-29 10:28:41 +0300

    update

--- dist/index.js
+++ dist/index.js
@@ -1,9 +1,9 @@
 "use strict";
 let age = 30_000_000;
-function nums(values) {
-    for (let i = 0; i < values.length; i++) {
-        console.log(values[i].toString());
-    }
+function nums(array) {
+    array.forEach((item) => {
+        console.log(item);
+    });
 }
 nums([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
 
 let age= 30_000_000;
 
-function nums(values:number[]) {
-    //loop through the array and convert each value to a string and log it
-    for (let i = 0; i < values.length; i++) {     
-        console.log(values[i].toString());
-    }
-}
+//function nums (array: number[]) {loops through the array and logs out each item}
 
+function nums (array: number[]) {
+    array.forEach((item) => {
+        console.log(item);
+    });
+}
 nums([1,2,3,4,5,6,7,8,9,10]);
